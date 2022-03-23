d7dfa75aa5ef3a36ff7660b87e44cf06bcc34a93
LiYanfeng <yanfeng413@163.com>
2022-03-23 22:11:42 +0800

    「优化」修改系统结构

--- shell/boot-admin.sh
+++ shell/boot-admin.sh
@@ -7,8 +7,6 @@ AppName=boot-admin.jar
 
 # JVM参数
 JVM_OPTS="-Dname=$AppName  -Duser.timezone=Asia/Shanghai -Xms512M -Xmx512M -XX:PermSize=256M -XX:MaxPermSize=512M -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
-APP_HOME=`pwd`
-LOG_PATH=$APP_HOME/logs/$AppName.log
 
 if [ "$1" = "" ];
 then
@@ -29,7 +27,7 @@ function start()
 	if [ x"$PID" != x"" ]; then
 	    echo "$AppName is running..."
 	else
-		nohup java -jar  $JVM_OPTS target/$AppName > /dev/null 2>&1 &
+		nohup java -jar  $JVM_OPTS $AppName > /dev/null 2>&1 &
 		echo "Start $AppName success..."
 	fi
 }
@@ -37,7 +35,7 @@ function start()
 function stop()
 {
     echo "Stop $AppName"
-	
+
 	PID=""
 	query(){
 		PID=`ps -ef |grep java|grep $AppName|grep -v grep|awk '{print $2}'`
